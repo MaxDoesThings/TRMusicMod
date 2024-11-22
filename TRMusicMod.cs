3b12ad6e95c0f94c333d3c8858991cbfff89eddb
MaxDoesThings <maxvillanueva2014@gmail.com>
2024-11-21 20:00:44 -0800

    Removed an un-needed test track.

--- TRMusicMod.cs
+++ TRMusicMod.cs
@@ -11,12 +11,12 @@ namespace TRMusicMod
             MusicSystem.MusicDict.Add("TRMusicMod/Tracks/thepainfulway", ModContent.Request<SoundEffect>("TRMusicMod/Tracks/thepainfulway"));
 
             MusicSystem.ForestTheme = new(
-                "TRMusicMod/Tracks/thepainfulway",
+                "TRMusicMod/Tracks/BaseThemeCalm",
                 "TerRoguelike/Tracks/BaseThemeCombat",
                 0.26f);
 
             MusicSystem.BaseTheme = new(
-                "TRMusicMod/Tracks/thepainfulway",
+                "TRMusicMod/Tracks/ForestThemeCalm",
                 "TerRoguelike/Tracks/ForestThemeCombat",
                 0.22f);
         }
